138592f5de4a97eca31bba35d9cd6c71ee775f70
DiscoverPioneer <phil@discoverPioneer.com>
2015-11-17 11:45:35 -0500

    added setup sql file

---
+++ setup.sql
@@ -0,0 +1,56 @@
+
+CREATE TABLE _User (
+  id SERIAL UNIQUE,
+  email VARCHAR(50) PRIMARY KEY,
+  password VARCHAR(50),
+  fname VARCHAR(50),
+  lname VARCHAR(50),
+  isAdmin VARCHAR(1)
+);
+
+CREATE TABLE Student (
+  id SERIAL PRIMARY KEY,
+  user_id integer UNIQUE REFERENCES _User(id),
+  year VARCHAR(20)
+);
+
+CREATE TABLE School (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  email_extension VARCHAR(20)
+);
+
+CREATE TABLE Course (
+  id SERIAL PRIMARY KEY,
+  course_number int,
+  course_title VARCHAR(100),
+  semester int
+);
+
+CREATE TABLE Takes (
+  id SERIAL PRIMARY KEY,
+  sid integer REFERENCES Student,
+  cid integer REFERENCES Course
+);
+
+CREATE TABLE Took (
+  id SERIAL PRIMARY KEY,
+  sid integer REFERENCES Student,
+  cid integer REFERENCES Course
+);
+
+CREATE TABLE File (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(75),
+  data bytea,
+  course integer REFERENCES Course,
+  owner integer REFERENCES _User(id),
+  date_created date
+);
+
+CREATE TABLE Chat_Messages (
+  id SERIAL PRIMARY KEY,
+  from_user integer REFERENCES _User(id),
+  to_user integer REFERENCES _User(id),
+  date_sent date
+);
